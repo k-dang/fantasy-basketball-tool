cef11badc1c1b515f4b7b72f1fa0fa7517fd0547
Kevin Dang <dangkevin12@gmail.com>
2025-11-02 20:19:09 -0500

    clean up more

--- src/types/yahoo.ts
+++ src/types/yahoo.ts
@@ -1,48 +1,48 @@
 export interface YahooUsersGamesLeaguesResponse {
-  fantasy_content: FantasyContent;
-}
-
-export interface FantasyContent {
-  users: Record<string, UserContainer>;
-}
-
-export interface UserContainer {
-  user: User[];
-  count?: number;
-}
-
-export interface User {
-  guid?: string;
-  games?: Omit<Record<string, GameContainer>, "count"> & {
-    count: number;
+  fantasy_content: {
+    users: Record<
+      string,
+      {
+        user: Array<{
+          guid?: string;
+          games?: Omit<Record<string, GameContainer>, "count"> & {
+            count: number;
+          };
+        }>;
+        count?: number;
+      }
+    >;
   };
 }
 
-export interface GameContainer {
-  game: [Game, GameLeagues];
-}
-
-export interface Game {
-  game_key: string;
-  game_id: string;
-  name: string;
-  season: string;
-  is_game_over: number;
-}
-
-interface GameLeagues {
-  leagues: LeaguesContainer;
-}
-
-export type LeaguesContainer = Omit<
-  Record<string, LeagueContainer>,
-  "count"
-> & {
-  count: number;
-};
-
-export interface LeagueContainer {
-  league: League[];
+interface GameContainer {
+  game: [
+    {
+      game_key: string;
+      game_id: string;
+      name: string;
+      season: string;
+      is_game_over: number;
+    },
+    {
+      leagues: Omit<
+        Record<
+          string,
+          {
+            league: Array<{
+              league_key: string;
+              league_id: string;
+              name: string;
+              season: string;
+            }>;
+          }
+        >,
+        "count"
+      > & {
+        count: number;
+      };
+    }
+  ];
 }
 
 export interface League {
@@ -52,50 +52,35 @@ export interface League {
   season: string;
 }
 
-export interface TeamLogo {
-  size: string;
-  url: string;
-}
+type TeamArrayElement = Record<string, unknown>;
 
-export interface TeamLogoContainer {
-  team_logo: TeamLogo;
-}
-
-export interface RosterAdds {
-  coverage_type: string;
-  coverage_value: number;
-  value: string;
-}
-
-export type TeamArrayElement = Record<string, unknown>;
-
-export type TeamArray = TeamArrayElement[];
-
-export interface TeamContainer {
-  team: [TeamArray];
-}
-
-export interface TeamsContainer {
-  teams: Omit<Record<string, TeamContainer>, "count"> & {
-    count: number;
-  };
+interface TeamContainer {
+  team: [Array<TeamArrayElement>];
 }
 
 export interface YahooLeagueTeamsResponse {
   fantasy_content: {
-    league: [League, TeamsContainer];
+    league: [
+      League,
+      {
+        teams: Omit<Record<string, TeamContainer>, "count"> & {
+          count: number;
+        };
+      }
+    ];
   };
 }
 
 export interface YahooTeamStatsResponse {
   fantasy_content: {
-    team: [TeamArray, TeamStats];
-  };
-}
-
-export interface TeamStats {
-  team_stats: {
-    stats: Array<StatContainer>;
+    team: [
+      Array<TeamArrayElement>,
+      {
+        team_stats: {
+          stats: Array<StatContainer>;
+        };
+      }
+    ];
   };
 }
 
@@ -103,62 +88,53 @@ export interface StatContainer {
   stat: Stat;
 }
 
-export interface Stat {
+interface Stat {
   stat_id: string;
   value: string;
 }
 
-export interface ExtendedStat extends Stat {
+interface ExtendedStat extends Stat {
   display_name: string;
 }
 
 export interface YahooTeamMatchupsResponse {
   fantasy_content: {
-    team: [TeamArray, TeamMatchups];
+    team: [
+      Array<TeamArrayElement>,
+      {
+        matchups: Omit<Record<string, MatchupContainer>, "count"> & {
+          count: number;
+        };
+      }
+    ];
   };
 }
 
-export interface TeamMatchups {
-  matchups: MatchupsContainer;
-}
-
-export type MatchupsContainer = Omit<
-  Record<string, MatchupContainer>,
-  "count"
-> & {
-  count: number;
-};
-
-export interface MatchupContainer {
-  matchup: Matchup;
-}
-
-export interface Matchup {
-  week: number | string;
-  week_start: string;
-  week_end: string;
-  status: string;
-  winner_team_key: string;
-  0: MatchupTeamContainer;
-}
-
-export interface MatchupTeamContainer {
-  teams: MatchupTeam;
-}
-
-export interface MatchupTeam {
-  0: MatchupTeamStatsContainer;
-  1: MatchupTeamStatsContainer;
-}
-
-export interface MatchupTeamStatsContainer {
-  team: [TeamArray, MatchupTeamStats];
-}
-
-export interface MatchupTeamStats {
-  team_stats: {
-    stats: Array<StatContainer>;
+interface MatchupContainer {
+  matchup: {
+    week: number | string;
+    week_start: string;
+    week_end: string;
+    status: string;
+    winner_team_key: string;
+    0: {
+      teams: {
+        0: MatchupTeamStatsContainer;
+        1: MatchupTeamStatsContainer;
+      };
+    };
   };
+}
+
+interface MatchupTeamStatsContainer {
+  team: [
+    Array<TeamArrayElement>,
+    {
+      team_stats: {
+        stats: Array<StatContainer>;
+      };
+    }
+  ];
 }
 
 export interface ParsedMatchup {
