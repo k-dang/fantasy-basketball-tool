8edc477979930a5ca13a2ed77521d996a9395ace
Kevin Dang <dangkevin12@gmail.com>
2025-11-12 19:17:31 -0500

    add stale time

--- src/components/QueryProvider.tsx
+++ src/components/QueryProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import * as React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, // 5 minutes - queries are fresh for 5 minutes
     },
   },
 });
